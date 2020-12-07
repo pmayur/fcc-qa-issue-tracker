e81d0c230267c9d92c9624e416bf9346cd009f60
pmayur <thisispatil@gmail.com>
2020-12-07 17:55:22 +0530

    add: Issue Model

---
+++ models/Issue.js
@@ -0,0 +1,38 @@
+// Require Mongoose
+const mongoose = require("mongoose");
+
+// Define the Issue Schema
+const IssueSchema = new mongoose.Schema(
+    {
+        issue_title: {
+            type: String,
+            required: true,
+        },
+        issue_text: {
+            type: String,
+            required: true,
+        },
+        created_by: {
+            type: String,
+            required: true,
+        },
+        assigned_to: {
+            type: String,
+            required: false,
+        },
+        status_text: {
+            type: String,
+            required: false,
+        },
+        open: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
+
+// Create a model from the schema
+const IssueModel = mongoose.model("IssueModel", IssueSchema);
+
+module.exports = IssueModel;
